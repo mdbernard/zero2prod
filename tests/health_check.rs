dd51055e11d45879356b6c7448338dbda35ce104
Mike Bernard <michael.bernard@uconn.edu>
2022-11-11 12:06:15 -0800

    add better tracing logs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,26 @@
 use std::net::TcpListener;
 
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+// ensure the `tracing` stack is only initialized once
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // create database
@@ -35,6 +51,9 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    // use one tracing stack for all tests
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
